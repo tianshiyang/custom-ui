4a9b4e91f76fbe8f489dbe905a91b4c931902adc
tianshiyang <tianshiyang@icourt.cc>
2023-05-29 22:47:03 +0800

    feat: 引入element

--- examples/App.vue
+++ examples/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
 </script>
 
 <style lang="scss">
---
+++ packages/myButton/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-button type="primary">按钮</el-button>
+</template>
+
+<script>
+export default {
+  name: 'MyButton'
+}
+</script>
+<script lang='ts' setup></script>
